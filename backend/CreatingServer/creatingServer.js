7c574b1dbe7e4f317c91d9b3f605e2326a172e8a
ShivpujanKr-NITJsr <sahil.8228053857@gmail.com>
2023-08-13 15:05:46 +0530

    getting url from req and based on that responding

--- backend/CreatingServer/creatingServer.js
+++ backend/CreatingServer/creatingServer.js
@@ -2,7 +2,32 @@ const http =require('http');
 
 
 const server=http.createServer((req,res)=>{
-    console.log("shivpujan",req);
+    // console.log(req.url, req.method,req.headers);
+    // // process.exit();
+    // res.setHeader('Content-Type', 'text/html');
+    // res.write('<html>');
+
+    // res.write('<head><title>My first Page</title></head>')
+
+    // res.write('<body><h1>Hello from my node.js server!</h1></body>')
+    // res.write('</html>')
+    // res.end();
+
+    const url=req.url;
+    if (url === '/home') {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('Welcome home');
+      } else if (url === '/about') {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('Welcome to About Us page');
+      } else if (url === '/node') {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('Welcome to my Node Js project');
+      } else {
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.end('404 - Not Found');
+      }
+
 });
 
 server.listen(4000)
