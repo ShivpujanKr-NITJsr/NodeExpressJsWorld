aed3aeab929e657aba69614d14ec4917164313a4
ShivpujanKr-NITJsr <sahil.8228053857@gmail.com>
2023-08-16 11:53:19 +0530

    implementing routers and filters

--- backend/ExpressProjet/app.js
+++ backend/ExpressProjet/app.js
@@ -1,23 +1,18 @@
-const express = require('express')
-const bodyParser= require('body-parser');
-const app=express();
+const express = require('express');
+const bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({extended:false}))
+const app = express();
 
-app.use('/add-product',(req,res,next)=>{
-    res.send('<form action="/product" method="POST"><input type="text" name="title" ><br><input type="text" name="quantity" ><button type="submit">Add Product</button></form>')
-    
-})
-app.post('/product',(req,res,next)=>{
-    console.log(req.body)
-    res.redirect('/');
-})
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/',(req,res,next)=>{
-    res.send('<h1> hello from express.js</h1>')
-    
+app.use('/admin',adminRoutes);
+app.use('/shop',shopRoutes);
+
+app.use((req,res,next )=>{
+    res.status(404).send('<h1> Page not found</h1>')
 })
 
-
-app.listen(3000)
+app.listen(3000);
