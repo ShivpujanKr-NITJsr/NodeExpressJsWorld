c7847ab70a86145cb8be5c8e9837fbb82021cea6
ShivpujanKr-NITJsr <sahil.8228053857@gmail.com>
2023-08-16 11:03:02 +0530

    used post method and form and body-parser to get value from user input

--- backend/ExpressProjet/app.js
+++ backend/ExpressProjet/app.js
@@ -1,20 +1,23 @@
-// const http = require('http');
-
 const express = require('express')
-
+const bodyParser= require('body-parser');
 const app=express();
 
-app.use((req,res,next)=>{
-    console.log('in the middleware')
-    next()            //allows the request to continue to the next middleware in line
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.use('/add-product',(req,res,next)=>{
+    res.send('<form action="/product" method="POST"><input type="text" name="title" ><br><input type="text" name="quantity" ><button type="submit">Add Product</button></form>')
+    
+})
+app.post('/product',(req,res,next)=>{
+    console.log(req.body)
+    res.redirect('/');
 })
 
-app.use((req,res,next)=>{
-    console.log('in another middleware')
+
+app.use('/',(req,res,next)=>{
     res.send('<h1> hello from express.js</h1>')
     
 })
 
-// const server = http.createServer(app);
-// server.listen(4000)
+
 app.listen(3000)
