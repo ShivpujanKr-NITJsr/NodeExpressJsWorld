bf0e55db5712f55bc4738211a00372aa8556616d
ShivpujanKr-NITJsr <sahil.8228053857@gmail.com>
2023-08-17 15:51:50 +0530

    added contact us in navbar and contactus.html and contact.js implementation for the action

--- backend/ExpressProjet/app.js
+++ backend/ExpressProjet/app.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path=require('path')
 
 const app = express();
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
+const contactus=require('./routes/contactus');
+
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/admin',adminRoutes);
 app.use('/shop',shopRoutes);
-
+app.use('/',contactus);
 app.use((req,res,next )=>{
-    res.status(404).send('<h1> Page not found</h1>')
+    
+    res.status(404).sendFile(path.join(__dirname,'views','error404.html'))
 })
 
 app.listen(3000);
---
+++ backend/ExpressProjet/routes/contactus.js
@@ -0,0 +1,18 @@
+const express=require('express');
+const path=require('path')
+const rootDir=require('../util/path')
+
+
+const router=express.Router();
+
+router.get('/contactus',(req,res,next)=>{
+    res.sendFile(path.join(rootDir,'views','getCall.html'))
+    
+
+})
+
+router.get('/success',(req,res,next)=>{
+    res.send('Form submitted successfully');
+})
+
+module.exports = router;
